f63b9bfee8560d1dbc84a1cf1d14b964d6d5e190
Andre Rodrigues <brodrigues405@gmail.com>
2020-01-02 23:00:14 -0300

    Versao Final

--- src/main/java/com/brodrigues/cadastro/model/Pessoa.java
+++ src/main/java/com/brodrigues/cadastro/model/Pessoa.java
@@ -7,6 +7,9 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
 @Entity
 @Table(name = "pessoa")
 public class Pessoa {
--- src/main/java/com/brodrigues/cadastro/resource/PessoaResource.java
+++ src/main/java/com/brodrigues/cadastro/resource/PessoaResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -11,14 +12,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.brodrigues.cadastro.model.Pessoa;
 import com.brodrigues.cadastro.repository.Pessoas;
-
-import net.bytebuddy.dynamic.scaffold.TypeWriter.FieldPool.Record;
 
 @CrossOrigin("*")
 @RestController
@@ -44,13 +44,45 @@ public class PessoaResource {
 		 return pessoa;
 	}
 	
-	@GetMapping( path = {"/{id}"})
-	public ResponseEntity<Pessoa> buscaPessoa(@PathVariable Long id) {
+	/*
+	 * @GetMapping( path = {"/{id}"}) public ResponseEntity<Pessoa>
+	 * buscaPessoa(@PathVariable Long id) {
+	 * 
+	 * return pessoas.findById(id) .map(record -> ResponseEntity.ok().body(record))
+	 * .orElse(ResponseEntity.notFound().build()); }
+	 */
+	
+	@GetMapping( path =  "/{id}")
+	public ResponseEntity<Pessoa> buscaPorID(@PathVariable Long id){
 		
-		return pessoas.findById(id)
-				.map(record -> ResponseEntity.ok().body(record))
-				.orElse(ResponseEntity.notFound().build());
+		Pessoa pessoa = pessoas.getOne(id);
+		
+		if(pessoa == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		return ResponseEntity.ok(pessoa);
+		
 	}
+	
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Pessoa> atualizar(@PathVariable Long id, @Valid @RequestBody Pessoa pessoa) {
+
+		Pessoa existente = pessoas.getOne(id);
+
+		if (existente == null) {
+			return ResponseEntity.notFound().build();
+		}
+
+		BeanUtils.copyProperties(pessoa, existente, "id");
+
+		existente = pessoas.save(existente);
+
+		return ResponseEntity.ok(existente);
+
+	}
+	
 	
 	
 	
